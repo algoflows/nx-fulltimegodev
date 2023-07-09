b8f55c4a7c84bcf2f424488b2769b130cfd91204
Sean Knowles <sknowles@onvutech.com>
2023-07-09 14:46:28 +0100

    feat: sections covered enums

--- apps/introgo/main.go
+++ apps/introgo/main.go
@@ -12,7 +12,7 @@ var (
 
 const (
 	version = 1
-	pi = 3.14159
+	pi      = 3.14159
 )
 
 func Hello(name string) string {
@@ -20,28 +20,37 @@ func Hello(name string) string {
 	return result
 }
 
+type WeaponType int
+
+const (
+	Axe WeaponType = iota // 0
+	Sword
+	WoodenStick
+	Knife
+)
+
 var (
-	floatVar float32 = 1.0 // 4 bytes (32 bits) of memory
-	floatVar2 float64 = 1.0 // default type for floating point numbers is float64 - 8 bytes (64 bits) of memory 
-	name string = "introgo" // string is a sequence of bytes
-	intVar32 int32 = 1 // 4 bytes (32 bits) of memory
-	intVar64 int64 = 1 // 8 bytes (64 bits) of memory
-	intVar int = 1 // default type for integers is int - 4 bytes (32 bits) of memory
-	uintVar uint = 1 // 4 bytes (32 bits) of memory
-	uintVar32 uint32 = 1 // 4 bytes (32 bits) of memory
-	uintVar64 uint64 = 1 // 8 bytes (64 bits) of memory
-	uint8Var uint8 = 1 // 1 byte (8 bits) of memory
-	uint16Var uint16 = 1 // 2 bytes (16 bits) of memory
-	uint32Var uint32 = 1 // 4 bytes (32 bits) of memory
-	uint64Var uint64 = 1 // 8 bytes (64 bits) of memory
-	byteVar byte = 1 // alias for uint8 - same as uint8Var
-	runeVar rune = 1 // alias for int32 - same as intVar32
-	runVar rune = 'a' // rune is an alias for int32 and represents a Unicode code point
+	floatVar  float32 = 1.0       // 4 bytes (32 bits) of memory
+	floatVar2 float64 = 1.0       // default type for floating point numbers is float64 - 8 bytes (64 bits) of memory
+	name      string  = "introgo" // string is a sequence of bytes
+	intVar32  int32   = 1         // 4 bytes (32 bits) of memory
+	intVar64  int64   = 1         // 8 bytes (64 bits) of memory
+	intVar    int     = 1         // default type for integers is int - 4 bytes (32 bits) of memory
+	uintVar   uint    = 1         // 4 bytes (32 bits) of memory
+	uintVar32 uint32  = 1         // 4 bytes (32 bits) of memory
+	uintVar64 uint64  = 1         // 8 bytes (64 bits) of memory
+	uint8Var  uint8   = 1         // 1 byte (8 bits) of memory
+	uint16Var uint16  = 1         // 2 bytes (16 bits) of memory
+	uint32Var uint32  = 1         // 4 bytes (32 bits) of memory
+	uint64Var uint64  = 1         // 8 bytes (64 bits) of memory
+	byteVar   byte    = 1         // alias for uint8 - same as uint8Var
+	runeVar   rune    = 1         // alias for int32 - same as intVar32
+	runVar    rune    = 'a'       // rune is an alias for int32 and represents a Unicode code point
 )
 
 type Player struct {
-	name string
-	health int
+	name        string
+	health      int
 	attackPower float64
 }
 
@@ -60,7 +69,7 @@ func main() {
 
 	version = 1
 	version = 2
-	version = 10 
+	version = 10
 
 	fmt.Println(Hello("introgo"))
 	fmt.Println("x:", x)
@@ -69,8 +78,8 @@ func main() {
 	fmt.Println("Pi:", pi)
 
 	player := Player{
-		name: "Captain America",
-		health: 100,
+		name:        "Captain America",
+		health:      100,
 		attackPower: 45.1,
 	}
 	fmt.Printf("Player: %+v\n", player)
@@ -97,13 +106,13 @@ func main() {
 	products["hat"] = 20.0
 	products = map[string]float64{
 		"shoes": 300.0,
-		"hat": 20.0,
+		"hat":   20.0,
 	}
 
 	users["Jack"] = 32
 	users["Jill"] = 30
 
-	fmt.Printf("Users: %+v\n", users)	
+	fmt.Printf("Users: %+v\n", users)
 	fmt.Printf("Users: %+v\n", users["John"])
 	fmt.Printf("Users: %+v\n", len(users))
 	fmt.Printf("Users: %+v\n", products)
@@ -113,7 +122,7 @@ func main() {
 		keys = append(keys, k)
 	}
 	fmt.Printf("Keys: %+v\n", keys)
-	
+
 	delete(users, "John")
 	fmt.Printf("Users: %+v\n", users)
 	for k, v := range users {
@@ -134,4 +143,38 @@ func main() {
 	fmt.Printf("Other numbers: %+v\n", otherNumbers)
 	clear(otherNumbers)
 
+	// enums
+	fmt.Printf("Damage of weapon (%s) (%d):\n ", Axe, getDamage(Axe))
+	fmt.Printf("Damage of weapon (%s) (%d):\n ", Sword, getDamage(Sword))
+	fmt.Printf("Damage of weapon (%s) (%d):\n ", WoodenStick, getDamage(WoodenStick))
+}
+
+func (w WeaponType) String() string {
+	switch w {
+	case Axe:
+		return "AXE"
+	case Sword:
+		return "SWORD"
+	case WoodenStick:
+		return "WOODEN_STICK"
+	case Knife:
+		return "KNIFE"
+	default:
+		return "UNKNOWN"
+	}
+}
+
+func getDamage(weaponType WeaponType) int {
+	switch weaponType {
+	case Axe:
+		return 100
+	case Sword:
+		return 90
+	case WoodenStick:
+		return 1
+	case Knife:
+		return 40
+	default:
+		panic("weapon does not exist")
+	}
 }
